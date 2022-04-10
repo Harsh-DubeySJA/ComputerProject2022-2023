2f14e00ea73f477537517d9f55486f28b238c501
Harsh Dubey <6350@saintjohnsacademy.com>
2022-04-10 15:44:12 +0530

    Raffle Question

---
+++ ProjectFiles/src/Raffle.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Raffle {
+    private final List<String> individuals = new ArrayList<>();
+    private List<String> winners;
+
+    Raffle (String fileName) {
+        try {
+        FileReader participants = new FileReader(fileName);
+        BufferedReader reader = new BufferedReader(participants);
+        String line;
+        while ((line = reader.readLine()) != null)
+            individuals.add(line);
+        reader.close();
+        participants.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File with " + fileName + " was not found");
+        }
+        catch (IOException e) {
+            System.out.println("NullPointerException");
+        }
+    }
+
+    private void draw(int prizeCount) {
+        winners = new ArrayList<>(prizeCount);
+        for (int i = 0; i < prizeCount; i++) {
+            String winner = individuals.get((int)(Math.random() * prizeCount) + prizeCount);
+            if (!winners.contains(winner))
+                winners.add(winner);
+        }
+    }
+
+    private void showWinners() {
+        System.out.println("Winners are \n------- ---\n");
+        for (String winner : winners)
+            System.out.println(winner);
+    }
+
+    public static void main(String[] args) {
+        Raffle obj = new Raffle("ProjectFiles/src/participants.csv");
+        obj.draw(5);
+        obj.showWinners();
+    }
+}
